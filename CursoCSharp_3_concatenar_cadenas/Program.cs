a4b2df480ac711cce75ace0fad2c0357e75ad396
saulvega <saul.vega.ext@oxxogas.com>
2020-08-01 19:24:44 -0500

    Fin sección 6 - Cadenas

---
+++ CursoCSharp_3_concatenar_cadenas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CursoCSharp_3_concatenar_cadenas {
+    class Program {
+        static void Main(string[] args) {
+            string superheore = "Superman";
+            string procedencia = "Kripton";
+            string superpoder = "super fuerza";
+
+            string todosDatos = superheore + " procede de " + procedencia + " y tiene " + superpoder;
+
+            string todosDatosConcat = string.Concat(superheore, " procede de ", procedencia, " y tiene ", superpoder);
+            string todosDatosConcat2 = string.Concat(superheore, superpoder);
+
+
+            string todosDatosInterpolation = $"{superheore} procede de {procedencia} y tiene {superpoder}";
+            string ejemploInterpolacion = $"{3 + 5} es igual a 8?";
+
+            //Console.WriteLine(todosDatos);
+            //Console.ReadLine();
+
+            //Console.WriteLine(todosDatosConcat);
+            //Console.WriteLine(todosDatosConcat2);
+
+            Console.WriteLine(todosDatosInterpolation);
+            Console.WriteLine(ejemploInterpolacion);
+            Console.ReadLine();
+        }
+    }
+}
