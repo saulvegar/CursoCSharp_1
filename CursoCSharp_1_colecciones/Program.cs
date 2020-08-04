d30c73c354e0074740a707f9a1683932f5f568e3
saulvega <saul.vega.ext@oxxogas.com>
2020-08-04 08:46:27 -0500

    Fin de la seccion 7 - Colecciones

---
+++ CursoCSharp_1_colecciones/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace CursoCSharp_1_colecciones {
+    class Program {
+        static void Main(string[] args) {
+            // ArrayList
+            ArrayList lista = new ArrayList();
+
+            // Pila o Stack
+            Stack miPila = new Stack();
+
+            // Cola o Queue
+            Queue cola = new Queue();
+
+            // Hashtable
+            Hashtable hastable = new Hashtable();
+        }
+    }
+}
---
+++ CursoCSharp_5_hashtable/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+
+namespace CursoCSharp_5_hashtable {
+    class Program {
+        static void Main(string[] args) {
+            Hashtable hashtable = new Hashtable();
+
+            // Añadir valores
+            hashtable.Add("Alejandro", 1.22);
+            hashtable.Add("Rodrigo", 5.21);
+            hashtable.Add("Miriam", 9.62);
+
+            // recorriendo Hasstable
+            foreach (DictionaryEntry item in hashtable) {
+                Console.WriteLine(item.Value + " " + item.Key);
+            }
+
+            // Obtener valor del hastable
+            var valor = hashtable["Alejandro"];
+
+            Console.WriteLine(valor);
+
+            Console.ReadLine();
+
+            // Cuantos elementos
+            var cuantos = hashtable.Count;
+
+            // buscar
+            if (hashtable.ContainsKey("Alejandro")) {
+
+            }
+
+            if (hashtable.ContainsValue(9.62)) {
+
+            }
+
+            // Eliminar
+            hashtable.Remove("Alejandro");
+
+            // recorriendo Hashtable
+            foreach (DictionaryEntry item in hashtable) {
+                Console.WriteLine(item.Value + " " + item.Key);
+            }
+
+            Console.ReadLine();
+
+            // Limpiar
+            hashtable.Clear();
+
+            // recorriendo Hashtabe
+            foreach (DictionaryEntry item in hashtable) {
+                Console.WriteLine(item.Value + " " + item.Key);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
